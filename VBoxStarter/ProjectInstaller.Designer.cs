c16b491de950fc7b4da081058d2b9013d08de86b
Gareth Stephenson <gareth.stephenson@gmail.com>
2015-10-12 22:46:35 +0200

    Initial commit to add default .gitIgnore and .gitAttribute files.

---
+++ VBoxStarter/ProjectInstaller.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace VBoxStarter
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VBoxStarterServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.VBoxStarterServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // VBoxStarterServiceProcessInstaller
+            // 
+            this.VBoxStarterServiceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.NetworkService;
+            this.VBoxStarterServiceProcessInstaller.Password = null;
+            this.VBoxStarterServiceProcessInstaller.Username = null;
+            // 
+            // VBoxStarterServiceInstaller
+            // 
+            this.VBoxStarterServiceInstaller.DelayedAutoStart = true;
+            this.VBoxStarterServiceInstaller.ServiceName = "VBoxStarterService";
+            this.VBoxStarterServiceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.VBoxStarterServiceProcessInstaller,
+            this.VBoxStarterServiceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller VBoxStarterServiceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller VBoxStarterServiceInstaller;
+    }
+}
---
+++ VBoxStarter/StarterService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace VBoxStarter
+{
+    public partial class StarterService : ServiceBase
+    {
+        public StarterService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
