3c584304b1c65e86ca8a73fe56aaf75443f48265
Gareth Stephenson <gareth.stephenson@dstvdm.com>
2015-10-13 13:29:00 +0200

    Added code to start a hidden process for each VM specified in the settings

--- VBoxStarter/Program.cs
+++ VBoxStarter/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.ServiceProcess;
-using System.Text;
+using Topshelf;
 
 namespace VBoxStarter
 {
@@ -13,12 +12,61 @@ namespace VBoxStarter
         /// </summary>
         static void Main()
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
-            {
-                new StarterService()
-            };
-            ServiceBase.Run(ServicesToRun);
+            HostFactory
+                .Run(hostConfigurator =>
+                {
+                    hostConfigurator.Service<IVBoxRunner>(serviceConfigurator =>
+                    {
+                        serviceConfigurator.ConstructUsing(() => new VBoxRunner());
+                        serviceConfigurator.WhenStarted(vBoxRunner => vBoxRunner.Start());
+                        serviceConfigurator.WhenStopped(vBoxRunner => vBoxRunner.Stop());
+                    });
+
+                    hostConfigurator.SetServiceName("VBoxStarterService");
+                    hostConfigurator.SetDisplayName("VBox Starter Service");
+                    hostConfigurator.SetDescription("A Windows Service to start VirtualBox instances in headless mode, on start up");
+
+                    hostConfigurator.RunAsNetworkService();
+                    hostConfigurator.StartAutomaticallyDelayed();
+                });
         }
+    }
+
+    internal class VBoxRunner : IVBoxRunner
+    {
+        public void Start()
+        {
+            if (Directory.Exists(Properties.Settings.Default.VirtualBoxPath))
+            {
+                if (File.Exists($@"{Properties.Settings.Default.VirtualBoxPath}\VBoxManage.exe"))
+                {
+                    var vmNames = Properties.Settings.Default.VMs.Split(',');
+                    foreach (var vmName in vmNames)
+                    {
+
+                        var processStartInfo = new ProcessStartInfo
+                        {
+                            CreateNoWindow = true,
+                            FileName = $@"{Properties.Settings.Default.VirtualBoxPath}\VBoxManage.exe",
+                            Arguments = $"startvm \"{vmName}\" --type headless",
+                            WindowStyle = ProcessWindowStyle.Hidden
+                        };
+                        Process.Start(processStartInfo);
+                    }
+                }
+            }
+        }
+
+        public void Stop()
+        {
+
+        }
+    }
+
+    internal interface IVBoxRunner
+    {
+        void Start();
+
+        void Stop();
     }
 }
